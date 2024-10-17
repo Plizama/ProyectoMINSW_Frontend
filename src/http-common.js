fde60ec55732b01c767772806d2f9a1d23f35bc6
Plizama <tuemail@example.com>
2024-10-17 18:14:32 -0300

    prueba front

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const miswBackendServer = import.meta.env.VITE_PROYECTO_MISW_BACKEND;
-const miswllBackendPort = import.meta.env.VITE_PROYECTO_MISW_PORT;
+const miswBackendServer = import.meta.env.produccion.VITE_PROYECTO_MISW_BACKEND;
+const miswllBackendPort = import.meta.env.produccion.VITE_PROYECTO_MISW_PORT;
 
 export default axios.create({
     baseURL: `http://${miswBackendServer}:${miswllBackendPort}`,
