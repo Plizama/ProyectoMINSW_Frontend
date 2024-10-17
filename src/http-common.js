ac6df403f8f9361bb3083ed93df8eb2e7de9393f
Plizama <tuemail@example.com>
2024-10-17 18:31:04 -0300

    recuperacion codigo anterior a prueba

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ const miswBackendServer = import.meta.env.VITE_PROYECTO_MISW_BACKEND;
 const miswllBackendPort = import.meta.env.VITE_PROYECTO_MISW_PORT;
 
 export default axios.create({
-    baseURL: 'http://20.206.160.177:8090',
+    baseURL: `http://${miswBackendServer}:${miswllBackendPort}`,
     headers: {
         'Content-Type': 'application/json'
     }
